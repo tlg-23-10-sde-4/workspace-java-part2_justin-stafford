e4f23a6ecbfda58048dcde38ca0e373d41fb719a
Justin Stafford <duckyabrams@gmail.com>
2023-11-08 14:57:20 -0800

    Lab7.1 Annotations and JavaDocs

---
+++ Lab07.2/src/com/javatunes/personnel/Employee.java
@@ -0,0 +1,32 @@
+/*
+ * This code is sample code, provided as-is, and we make no
+ * warranties as to its correctness or suitability for
+ * any purpose.
+ *
+ * We hope that it's useful to you.  Enjoy.
+ * Copyright LearningPatterns Inc.
+ */
+package com.javatunes.personnel;
+
+import com.fastjava.annotation.Fast;
+
+/**
+ * TODO: all methods in this class should run 25% faster.
+ */
+//@Fast(boost=0.90)
+public class Employee {
+
+    /**
+     * TODO: this method needs to run 50% faster.
+     * OSHA regulations require extra logging about the performance improvement,
+     * as well as a description of why we're using this technology from FastJava.com.
+     */
+    @Fast(boost=0.75, log=true, description="Employee need to work harder!!!")
+    public void work() {
+        System.out.println("Employee working hard");
+    }
+
+    public void pay() {
+        System.out.println("Employee paid well");
+    }
+}
