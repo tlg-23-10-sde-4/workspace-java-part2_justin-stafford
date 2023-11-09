a06b719260d4b143dd2ae0f69daecdd81865cdfc
Justin Stafford <duckyabrams@gmail.com>
2023-11-09 10:44:04 -0800

    Lab8.1 add Lambda to Repeating Printer

---
+++ Lab08.1/test/com/javatunes/thread/RepeatingPrinterTest.java
@@ -0,0 +1,31 @@
+/*
+ * This code is sample code, provided as-is, and we make no
+ * warranties as to its correctness or suitability for
+ * any purpose.
+ *
+ * We hope that it's useful to you.  Enjoy.
+ * Copyright LearningPatterns Inc.
+ */
+package com.javatunes.thread;
+
+public class RepeatingPrinterTest {
+
+  public static void main(String[] args) {
+    Thread thd1 = new Thread(new RepeatingPrinter());
+    //thd1.start();
+
+    // TODO: initialize the 'printer' reference variable with a block lambda
+    // hint: you can copy / paste the *contents* of RepeatingPrinter's run() method
+    // note: change the sysout to show "block-lambda"
+    Thread thd2 = new Thread() ->{
+      while (true) {
+        System.out.println("RepeatingPrinter.run");
+        try {
+          Thread.sleep(1000);
+        } catch (InterruptedException e) {
+        }
+      }
+      thd2.start();
+      // TODO: create another Thread, passing in the 'printer' reference as its Runnable - then start it
+    }
+  }
---
+++ Lab08.1/test/com/javatunes/thread/SimplePrinterTest.java
@@ -0,0 +1,23 @@
+/*
+ * This code is sample code, provided as-is, and we make no
+ * warranties as to its correctness or suitability for
+ * any purpose.
+ *
+ * We hope that it's useful to you.  Enjoy.
+ * Copyright LearningPatterns Inc.
+ */
+package com.javatunes.thread;
+
+public class SimplePrinterTest {
+
+  public static void main(String[] args) {
+    Thread thd1 = new Thread(new SimplePrinter());
+    thd1.start();
+
+      // Create another Thread, passing in a lambda as its Runnable
+      Thread thd2 = new Thread(() -> System.out.println("Lambda Printer.run"));
+      thd2.start();
+
+    // TODO: create another Thread, passing in a lambda as its Runnable - then start it
+  }
+}
